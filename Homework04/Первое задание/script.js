dc7c4db5e8a6c2eae7d691f9472f0a4de536618a
Plyton <emenevr@mail.ru>
2020-06-11 21:39:01 +0400

    Homework4(fixed)

--- Homework04/Первое задание/script.js
+++ Homework04/Первое задание/script.js
@@ -3,13 +3,26 @@ let numberRandom = String(Math.floor(Math.random() * 999))
 
 function getObjectFromNumber(number) {
     let resultingObject = {}
-    if ((number < 0 && number > 999) || !Number.isInteger(Number(number)) || isNaN(number)) {
+    if (number < 0 || number > 999 || !Number.isInteger(Number(number)) || isNaN(number)) {
         return resultingObject
     } else {
-        resultingObject.hundreds = Number(number[0])
-        resultingObject.tens = number[1] === undefined ? 0 : Number(number[1])
-        resultingObject.units = number[2] === undefined ? 0 : Number(number[2])
-        return resultingObject
+        if (number.length == 3) {
+            resultingObject.hundreds = Number(number[0])
+            resultingObject.tens = Number(number[1])
+            resultingObject.units = Number(number[2])
+            return resultingObject
+        } else if (number.length == 2) {
+            resultingObject.hundreds = 0
+            resultingObject.tens = Number(number[0])
+            resultingObject.units = Number(number[1])
+            return resultingObject
+        } else if (number.length == 3) {
+            resultingObject.hundreds = 0
+            resultingObject.tens = 0
+            resultingObject.units = Number(number[0])
+            return resultingObject
+        }
+
     }
 
 }
