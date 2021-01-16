32bec19b5ac537bfe72e0a7d020f1a10c4961c73
Plyton <emenevr@mail.ru>
2021-01-17 00:26:20 +0400

    Homework08(version1.0)

--- Homework07/snake/app.js
+++ Homework07/snake/app.js
@@ -48,6 +48,11 @@ class Status {
     setPaused() {
         this.status = 'paused';
     }
+
+    setGameOver() {
+        this.status = 'game over'
+    }
+
     isPlaying() {
         return this.status === 'playing';
     }
@@ -477,6 +482,7 @@ class Game {
         if (this.board.isNextStepBody(this.snake.bodySnake[0]) || this.board.isNextStepBomb(this.snake.bodySnake[0])) {
             clearInterval(this.tickIdentifier);
             this.setMessage('Вы проиграли');
+            this.status.setGameOver()
             return true;
         }
         return false;
@@ -486,6 +492,7 @@ class Game {
         if (this.snake.bodySnake.length === this.settings.winlength) {
             clearInterval(this.tickIdentifier);
             this.setMessage('Вы выиграли');
+            this.status.setGameOver()
             return true;
         }
         return false;
